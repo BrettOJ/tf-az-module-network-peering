ea8155f267cb337691b737880777d6c7ac8c3fc0
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2024-11-12 11:02:27 +0800

    initial commit

--- example/vnet-peering.tf
+++ example/vnet-peering.tf
@@ -35,8 +35,8 @@ module "azure_virtual_network_peering" {
   allow_forwarded_traffic      = var.allow_forwarded_traffic
   allow_gateway_transit        = var.allow_gateway_transit 
   use_remote_gateways          = var.use_remote_gateways
-  local_subnet_names           = var.local_subnet_names
-  remote_subnet_names          = var.remote_subnet_names
+  local_subnet_names           = [var.local_subnet_names]
+  remote_subnet_names          = [var.remote_subnet_names]
   only_ipv6_peering_enabled    = var.only_ipv6_peering_enabled
   triggers = var.triggers
 }
